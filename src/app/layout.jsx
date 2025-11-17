954a388022d920a40c9921adb5ccecf6783531ac
sandhyamainali <sandhyamainali55@gmail.com>
2025-11-17 22:26:32 +0545

    transaction

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="antialiased" suppressHydrationWarning={true}>
         <UserProvider>
           <CartProvider>
             <Navbar />
